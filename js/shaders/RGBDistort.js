83aef84d718920e1a294b1573ff4e51fc32774de
Jo Emil Holen <jo.emil@skriveleif.com>
2013-03-16 15:01:53 +0100

    Errorfixing after merge

--- js/shaders/RGBDistort.js
+++ js/shaders/RGBDistort.js
@@ -1,28 +1,14 @@
 THREE.RGBDistortShader = {
     uniforms: {
-<<<<<<< HEAD
         "tDiffuse": {type: "t", value:null},
-        "amount": {type: "f", value: 0.},
-        "angle": {type: "f", value: 0.}
-=======
-        // General uniforms
-        "tDiffuse": {type: "t", value: null},
-        "time": {type: "f", value:.0},
-
-        // Uniforms for distorting colors
         "rgbAmt": {type: "f", value:.005},
         "rgbAng": {type: "f", value:1.}
->>>>>>> distort
     },
 
     vertexShader: [
         "varying vec2 vUv;",
-<<<<<<< HEAD
-        "void main() {",
-=======
         "void main() ",
         "{",
->>>>>>> distort
             "vUv = uv;",
             "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
         "}"
@@ -30,20 +16,6 @@ THREE.RGBDistortShader = {
 
     fragmentShader: [
         "uniform sampler2D tDiffuse;",
-<<<<<<< HEAD
-        "uniform float amount;",
-        "uniform float angle;",
-
-        "varying vec2 vUv;",
-
-        "void main() {",
-            "vec2 offset = amount * vec2(cos(angle), sin(angle));",
-            "vec4 cr = texture2D(tDiffuse, vUv + offset);",
-            "vec4 cga = texture2D(tDiffuse, vUv);",
-            "vec4 cb = texture2D(tDiffuse, vUv - offset);",
-            "gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);",
-=======
-        "uniform float time;",
         "uniform float rgbAmt;",
         "uniform float rgbAng;",
         "varying vec2 vUv;",
@@ -60,7 +32,6 @@ THREE.RGBDistortShader = {
             "color.r = cr.r; color.g = cga.g; color.b = cb.b; color.a = cga.a;",
 
             "gl_FragColor = color;",
->>>>>>> distort
         "}"
     ].join("\n")
 };
