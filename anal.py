4283c27c6297c6b82fb4991b121bca19bee5fb69
Jo Emil Holen <jo.emil@skriveleif.com>
2013-03-18 09:31:19 +0100

    Added image analyzer, not sure if needed though

---
+++ anal.py
@@ -0,0 +1,18 @@
+import os,sys
+import Image, json
+
+im = Image.open("image.png")
+
+width = im.size[0]
+height = im.size[1]
+
+pix = im.load()
+pixels = []
+for x in range(0, width):
+	pixels.append([])
+	for y in range(0, height):
+		pixels[x].append(list(pix[x,y]))
+
+txt = open("image.json", "w")
+json.dump(pixels, txt)
+txt.close()
