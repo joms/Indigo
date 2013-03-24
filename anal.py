073a33a13e2bf8f8e30a91ef8fcd61f26ac2486e
Jo Emil Holen <jo.emil@skriveleif.com>
2013-03-24 23:59:26 +0100

    Nite nite! Misa is happy!

--- anal.py
+++ anal.py
@@ -8,9 +8,10 @@ height = im.size[1]
 
 pix = im.load()
 pixels = []
-for x in xrange(width, 0, -1):
+
+for x in range(0, width):
 	pixels.append([])
-	for y in xrange(height, 0, -1):
+	for y in range(0, height):
 		pixels[x].append(list(pix[x,y]))
 
 txt = open("image.json", "w")
